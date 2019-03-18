1cc9d6fb4408d39e1a809de382dadd4cb902de37
Pramod2903 <prakashooty16@gmail.com>
2019-03-18 04:11:17 -0700

    crossOriginUpdte

--- src/main/java/com/usecase/mortgage/controller/UserController.java
+++ src/main/java/com/usecase/mortgage/controller/UserController.java
@@ -3,6 +3,7 @@ package com.usecase.mortgage.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.usecase.mortgage.service.UserService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class UserController {
 	@Autowired
 	UserService userService;
@@ -30,5 +32,7 @@ public class UserController {
 		User user = userService.getUserByUserName(userName);
 		return new ResponseEntity<>(user,HttpStatus.OK);
 	}
+	
+	
 
 }
