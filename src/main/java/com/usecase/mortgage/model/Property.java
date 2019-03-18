125572d4da630d6e3eea35232207c4315e97883d
prakashsubramanii <prakashooty16.com>
2019-03-19 04:37:12 +0530

    Entity class commit

--- src/main/java/com/usecase/mortgage/model/Property.java
+++ src/main/java/com/usecase/mortgage/model/Property.java
@@ -13,5 +13,48 @@ public class Property {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
+	private String propertyName;
+	private String type;
+	private double area;
+	private Long pincode;
+	private String address;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getPropertyName() {
+		return propertyName;
+	}
+	public void setPropertyName(String propertyName) {
+		this.propertyName = propertyName;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public double getArea() {
+		return area;
+	}
+	public void setArea(double area) {
+		this.area = area;
+	}
+	public Long getPincode() {
+		return pincode;
+	}
+	public void setPincode(Long pincode) {
+		this.pincode = pincode;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
 
 }
--- src/main/java/com/usecase/mortgage/model/User.java
+++ src/main/java/com/usecase/mortgage/model/User.java
@@ -1,5 +1,7 @@
 package com.usecase.mortgage.model;
 
+import java.util.Date;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,8 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import antlr.collections.List;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table
@@ -22,5 +24,86 @@ public class User {
 	@OneToMany(cascade=CascadeType.ALL)
 	@JoinColumn(name="property_id")
 	private Property property;
+	private String name;
+	private String password;
+	private String cpassword;
+	private Long phone;
+	private String email;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dob;
+	private String gender;
+	private String pan;
+	private double salary;
+	private double altIncomee;
+	private double otherIncome;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCpassword() {
+		return cpassword;
+	}
+	public void setCpassword(String cpassword) {
+		this.cpassword = cpassword;
+	}
+	public Long getPhone() {
+		return phone;
+	}
+	public void setPhone(Long phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPan() {
+		return pan;
+	}
+	public void setPan(String pan) {
+		this.pan = pan;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public double getAltIncomee() {
+		return altIncomee;
+	}
+	public void setAltIncomee(double altIncomee) {
+		this.altIncomee = altIncomee;
+	}
+	public double getOtherIncome() {
+		return otherIncome;
+	}
+	public void setOtherIncome(double otherIncome) {
+		this.otherIncome = otherIncome;
+	}
+	
+	
 	
 }
