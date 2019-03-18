aecbd874af5c43f9389dea7f37e8ac6b0163e5b5
Pramod2903 <prakashooty16@gmail.com>
2019-03-18 05:54:40 -0700

    commit 6

--- src/main/java/com/usecase/mortgage/service/OfferService.java
+++ src/main/java/com/usecase/mortgage/service/OfferService.java
@@ -1,8 +1,16 @@
 package com.usecase.mortgage.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.usecase.mortgage.model.Offer;
 
 @Service
 public interface OfferService {
+
+	public List<Offer> getAllOffers();
+
+	public List<Offer> getEligibleOffers(double offerValue);
 
 }
--- src/main/java/com/usecase/mortgage/service/UserServiceImpl.java
+++ src/main/java/com/usecase/mortgage/service/UserServiceImpl.java
@@ -1,12 +1,16 @@
 package com.usecase.mortgage.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.time.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.usecase.mortgage.exceptions.UserNotFoundException;
+import com.usecase.mortgage.model.Offer;
 import com.usecase.mortgage.model.Property;
 import com.usecase.mortgage.model.User;
 import com.usecase.mortgage.repository.UserRepository;
@@ -16,6 +20,10 @@ public class UserServiceImpl implements UserService{
 
 	@Autowired
 	UserRepository userRepository;
+	@Autowired
+	SqftService sqftService;
+	@Autowired
+	OfferService offerService;
 	
 	@Override
 	public User createUser(User user) {
@@ -32,7 +40,8 @@ public class UserServiceImpl implements UserService{
 		
 	}
 	
-	public void getOffers(String userName) {
+	public Map<Long, List<Offer>> getOffers(String userName) throws Exception {
+		Map<Long, List<Offer>> offers = new HashMap();
 		User user = userRepository.findByUserName(userName)
 				.orElseThrow(()->new UserNotFoundException("No User found with username :"+userName));
 		Double baseAmount = (user.getSalary() + user.getOtherIncome()+user.getAltIncomee())*0.4;
@@ -46,11 +55,13 @@ public class UserServiceImpl implements UserService{
 		}
 		
 		for(Property prop : props) {
-			//prop.getPincode()
+			Double propValue = prop.getArea() * sqftService.getSqftValue(prop.getPincode()).getValue();
 			
+			Double offerValue = propValue*0.6;
+			offers.put(prop.getId(), offerValue<1000000 ? null : offerService.getEligibleOffers(offerValue));
 			
 		}
-		
+		return offers;
 		
 		
 	}
