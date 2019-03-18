ac458fc2a44b4076c7c1795c52f5f57a82f958df
Pramod2903 <prakashooty16@gmail.com>
2019-03-18 05:39:18 -0700

    copmmit 5

--- src/main/java/com/usecase/mortgage/service/UserServiceImpl.java
+++ src/main/java/com/usecase/mortgage/service/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.usecase.mortgage.service;
 
+import java.util.List;
+import java.time.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.usecase.mortgage.exceptions.UserNotFoundException;
+import com.usecase.mortgage.model.Property;
 import com.usecase.mortgage.model.User;
 import com.usecase.mortgage.repository.UserRepository;
 
@@ -21,6 +25,29 @@ public class UserServiceImpl implements UserService{
 	public User getUserByUserName(String userName) {
 		return userRepository.findByUserName(userName)
 				.orElseThrow(()->new UserNotFoundException("No User found with username :"+userName));
+	}
+	
+	public void getOffers(String userName) {
+		User user = userRepository.findByUserName(userName)
+				.orElseThrow(()->new UserNotFoundException("No User found with username :"+userName));
+		Double baseAmount = (user.getSalary() + user.getOtherIncome()+user.getAltIncomee())*0.4;
+		
+		List<Property> props = user.getProperties();
+		ZoneId defaultZoneId = ZoneId.systemDefault();
+		Instant instant = user.getDob().toInstant();		
+		Period period = Period.between(LocalDate.now(), instant.atZone(defaultZoneId).toLocalDate());
+		if(period.getYears()>60 || baseAmount <15000){
+			//return null;
+		}
+		
+		for(Property prop : props) {
+			//prop.getPincode()
+			
+			
+		}
+		
+		
+		
 	}
 	
 	
--- src/test/java/com/usecase/mortgage/repository/UserRepositoryTest.java
+++ src/test/java/com/usecase/mortgage/repository/UserRepositoryTest.java
@@ -1,10 +1,36 @@
 package com.usecase.mortgage.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.usecase.mortgage.model.User;
 
-public interface UserRepositoryTest extends JpaRepository<User,Long>{
-	
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserRepositoryTest {
 
+	@Autowired
+	TestEntityManager entityManager;
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Test
+	public void tset() throws Exception{
+		User user = new User();
+		user.setName("Test");
+		user.setUserName("Test23");
+		user.setPassword("p@ssWord245");
+		user.setPan("BVC5578G");
+		entityManager.persist(user);
+		User savedUser = userRepository.findByUserName("Test23").orElseThrow(()->new Exception());
+		
+		assertThat(user.getName()).isEqualTo("Test");		
+	}
 }
